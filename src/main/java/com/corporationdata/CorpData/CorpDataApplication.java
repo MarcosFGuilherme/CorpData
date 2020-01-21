c2eee07859c2b367aa1933d7f97ee1006a5e1cc6
Marcos F S Guilherme <marcosfs.guilherme@gmail.com>
2020-01-21 13:15:11 -0300

    000-Projeto Criado

---
+++ src/main/java/com/corporationdata/CorpData/CorpDataApplication.java
@@ -0,0 +1,13 @@
+package com.corporationdata.CorpData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorpDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorpDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corporationdata/CorpData/CorpDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corporationdata.CorpData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorpDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
